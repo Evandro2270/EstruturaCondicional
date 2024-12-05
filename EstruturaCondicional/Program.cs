a5c27a9466dbf493f0bf7a4032527b522c2cb802
Evandro2270 <evandro535@gmail.com>
2024-12-05 09:04:55 -0300

    Adicionar arquivos de projeto.

---
+++ EstruturaCondicional/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+namespace Programa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+
+                CultureInfo CI = CultureInfo.InvariantCulture;
+                Console.WriteLine("ente com numero par: ");
+                int x = int.Parse(Console.ReadLine());
+
+                if (x % 2 == 0 ) {
+                    Console.WriteLine("par!");
+                }
+                else
+                {
+                    Console.WriteLine("impar!");
+                }
+            }
+        }
+    }
+}
